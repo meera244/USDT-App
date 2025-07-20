089d65c829a9c62fd326f6c8d3d8fbb0f2404081
meera244 <meeravahane@gmail.com>
2025-07-20 15:16:46 +0530

    Update layuot.js

--- app/layuot.js
+++ app/layuot.js
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, fontFamily: 'sans-serif', backgroundColor: '#111' }}>
+      <body style={{ margin: 0}}>
         {children}
       </body>
     </html>
